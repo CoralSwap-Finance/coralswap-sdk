97d8a5c3febd0e5d87d13d25c6ff3bb2f8d788d7
isaac4real-art <Isaacoluwagbamigbe@gmail.com>
2026-07-29 13:10:55 +0100

    feat: schema helpers

--- src/schemas/swap.ts
+++ src/schemas/swap.ts
@@ -3,3 +3,7 @@ import { isValidAddress } from '../utils/addresses';
 import { TradeType } from '../types/common';
 import { ValidationError } from '../errors';
 
+const stellarAddress = z.string().min(1, { message: 'must not be empty' }).refine(
+  (val) => isValidAddress(val),
+  (val) => ( { message: `is not a valid Stellar address: ${val}` }),
+);
