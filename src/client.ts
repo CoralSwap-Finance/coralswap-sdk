2d65cb2c816bde05a45858a632d84f3eed12b0bf
Larry <odesanyalanre3@gmail.com>
2026-08-04 12:15:56 +0100

    fix(client): use server setter in executeWithFallback for mock server support

--- src/client.ts
+++ src/client.ts
@@ -75,7 +75,7 @@ export class CoralSwapClient {
       const rpcUrl = this._connectionPool.getEndpoint();
 
       if (rpcUrl !== this._activeRpcUrl) {
-        this._server = this.createRpcServer(rpcUrl);
+        this.server = this.createRpcServer(rpcUrl);
         this._poller = null;
         this._activeRpcUrl = rpcUrl;
         this.networkConfig.rpcUrl = rpcUrl;
